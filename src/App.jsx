e2a2f7acf061b709e42626c302031668d5961d3a
Jeong mokjin <jungmokjin@gmail.com>
2026-04-21 15:37:04 +0900

    ✨ Feat: router 및 API 템플릿 적용, 기본 레이아웃 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import RootLayout from '@/layouts/RootLayout';
+import ServiceLayout from '@/layouts/ServiceLayout';
+import ProtectedRoute from '@/router/ProtectedRoute';
+
+//const page = (importFn) => () => importFn().then((m) => ({ Component: m.default }));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 1 },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    Component: RootLayout,
+    children: [
+      {
+        Component: ProtectedRoute,
+        children: [
+          //{ path: "", lazy: page(() => import("파일 경로")) },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    Component: ServiceLayout,
+    children: [
+      //{ index: true, lazy: page(() => import("파일 경로")) },
+      //{ path: "", lazy: page(() => import("파일 경로")) },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
