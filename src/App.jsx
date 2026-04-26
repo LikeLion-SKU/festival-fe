5c3e32b45acfb0f91c8f6cca4ec605661fa14ef7
simGPT <simGPT02@gmail.com>
2026-04-26 17:09:52 +0900

    :sparkles:Feat: Order 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MobileLayout from '@/layouts/MobileLayout';
 import RootLayout from '@/layouts/RootLayout';
 import ServiceLayout from '@/layouts/ServiceLayout';
 import Main from '@/pages/Main';
+import Order from '@/pages/Order/Order';
 import ProtectedRoute from '@/router/ProtectedRoute';
 
 const page = (importFn) => () => importFn().then((m) => ({ Component: m.default }));
@@ -28,7 +29,15 @@ const router = createBrowserRouter([
       {
         Component: MobileLayout,
         children: [
-          //{ path: '', lazy: page(() => import('파일 경로')) },
+          {
+            path: '/order',
+            Component: Order,
+            children: [
+              { index: true, lazy: page(() => import('@/pages/Order/OrderEntry')) }, // order 접속 시 자동으로 OrderEntry부터 기본으로
+              //{ path: 'confirm', lazy: page(() => import('@/pages/Order/OrderConfirm')) },
+              //{ path: 'complete', lazy: page(() => import('@/pages/Order/OrderComplete')) },
+            ],
+          },
         ],
       },
       {
