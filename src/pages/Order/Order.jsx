5113e795b0ee370c027c40592fd8d99212547003
simGPT <simGPT02@gmail.com>
2026-04-26 17:13:52 +0900

    WIP : 퍼블리싱 미완료, develop pull 전 임시 커밋

---
+++ src/pages/Order/Order.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from 'react-router';
+
+function Order() {
+  return <Outlet />; // 자식 라우트 그려질 자리
+}
+
+export default Order;
---
+++ src/pages/Order/OrderEntry.jsx
@@ -0,0 +1,11 @@
+import Booth from '@/assets/images/booth_image.svg?react';
+
+function OrderEntry() {
+  return (
+    <>
+      <Booth className="w-49 h-49" />
+    </>
+  );
+}
+
+export default OrderEntry;
