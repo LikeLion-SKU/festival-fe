51467e2567c085a4c9489ed2a0dc02f7e568316c
simGPT <simGPT02@gmail.com>
2026-04-27 11:32:05 +0900

    :sparkles:Feat: 주문페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -81,6 +81,7 @@ const router = createBrowserRouter([
         Component: Order,
         children: [
           { index: true, lazy: page(() => import('@/pages/Order/OrderEntry')) }, // order 접속 시 자동으로 OrderEntry부터 기본으로
+          { path: 'progress', lazy: page(() => import('@/pages/Order/OrderProgress')) },
           //{ path: 'confirm', lazy: page(() => import('@/pages/Order/OrderConfirm')) },
           //{ path: 'complete', lazy: page(() => import('@/pages/Order/OrderComplete')) },
         ],
