6b0b9079423018177f37d07710ccc108feace07e
simGPT <simGPT02@gmail.com>
2026-04-27 15:28:14 +0900

    :art:Design: OrderButtonBox 최대길이 450px 제한

--- src/components/Order/OrderEntry/OrderButtonBox.jsx
+++ src/components/Order/OrderEntry/OrderButtonBox.jsx
@@ -7,7 +7,7 @@ function OrderButtonBox({
   onClick,
 }) {
   return (
-    <div className="w-full h-25 fixed bottom-0 left-1/2 -translate-x-1/2 z-50 shadow-[0px_-1px_7px_-2px_rgba(0,0,0,0.25)] bg-white px-7 pt-3.75">
+    <div className="w-full max-w-112.5 h-25 fixed bottom-0 left-1/2 -translate-x-1/2 z-50 shadow-[0px_-1px_7px_-2px_rgba(0,0,0,0.25)] bg-white px-7 pt-3.75">
       <OrderButton
         width="100%"
         height={48}
