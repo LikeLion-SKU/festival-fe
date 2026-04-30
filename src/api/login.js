e530ba0de3686c62341348d142fa0941ea88187d
Jeong mokjin <jungmokjin@gmail.com>
2026-04-30 22:32:00 +0900

    ✨ Feat: 로그인 api 연결 및 api 토큰 관리 수정

--- src/api/login.js
+++ src/api/login.js
@@ -1,14 +1,10 @@
 import { APIService } from '@/api/api';
 
 export const postLogin = async (departmentName, password) => {
-  try {
-    const res = APIService.public.post('/auth/login', {
-      departmentName,
-      password,
-    });
+  const res = await APIService.public.post('/auth/login', {
+    departmentName,
+    password,
+  });
 
-    return res;
-  } catch (error) {
-    console.log('로그인 실패' + error);
-  }
+  return res;
 };
