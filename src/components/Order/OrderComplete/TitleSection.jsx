e34eec58aa71395c7bf7548cb89f88067a429422
simGPT <simGPT02@gmail.com>
2026-04-30 14:27:39 +0900

    :art:Design: 주문 완료 페이지 퍼블리싱 완료

--- src/components/Order/OrderComplete/TitleSection.jsx
+++ src/components/Order/OrderComplete/TitleSection.jsx
@@ -2,10 +2,10 @@ import CheckIcon from '@/assets/icons/check_orange.svg?react';
 
 function TitleSection() {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 py-16 bg-gray-bg">
-      <CheckIcon className="w-16 h-16" />
-      <div className="flex flex-col items-center gap-2">
-        <p className="text-xl font-bold text-order-button">주문 요청이 완료됐어요!</p>
+    <div className="flex flex-col items-center justify-center gap-2 py-16 bg-gray-bg pb-5">
+      <CheckIcon className="w-10.5 h-10.5" />
+      <div className="flex flex-col items-center gap-1">
+        <p className="text-xl font-semibold text-order-button">주문 요청이 완료됐어요!</p>
         <p className="text-sm font-medium text-gray-400">입금 확인 후 조리가 시작돼요</p>
       </div>
     </div>
