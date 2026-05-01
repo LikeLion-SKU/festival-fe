10ea724ea09518f1155082db4a2762db3cfe8021
hyeonsu_ <117705439+agness4011@users.noreply.github.com>
2026-05-01 21:40:13 +0900

    🎨 Design: 부스 안내도 은주1관 컴포넌트 추가

--- src/components/Booth/BoothMap.jsx
+++ src/components/Booth/BoothMap.jsx
@@ -1,3 +1,4 @@
+import Eunju1 from '@/components/Booth/Eunju1';
 import Eunju2 from '@/components/Booth/Eunju2';
 import Hyein from '@/components/Booth/Hyein';
 
@@ -10,6 +11,12 @@ export default function BoothMap({ activeBuildingId, onSelectBuilding }) {
     <div className="relative aspect-[335/360] w-full bg-[#121212]">
       <div className="pointer-events-auto absolute left-1/2 top-[8%] z-10 -translate-x-1/2">
         <Hyein active={activeBuildingId === 'hyein'} onClick={() => onSelectBuilding?.('hyein')} />
+      </div>
+      <div className="pointer-events-auto absolute left-[18%] top-[34%] z-10">
+        <Eunju1
+          active={activeBuildingId === 'eunju1'}
+          onClick={() => onSelectBuilding?.('eunju1')}
+        />
       </div>
       <div className="pointer-events-auto absolute left-[59%] top-[36%] z-10">
         <Eunju2
