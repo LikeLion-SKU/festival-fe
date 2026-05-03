9530eb90ab2a22a98659013b5332d80e936fe0a7
Jeong mokjin <jungmokjin@gmail.com>
2026-05-03 21:01:01 +0900

    ✨ Feat: 루트 및 모바일 레이아웃 구조 수정, 로딩 컨텍스트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import AdminLayout from '@/layouts/AdminLayout';
 import MobileLayout from '@/layouts/MobileLayout';
-import RootLayout from '@/layouts/RootLayout';
 import Order from '@/pages/Order/Order';
 import AdminMain from '@/pages/admin/AdminMain';
 
@@ -18,23 +17,33 @@ const queryClient = new QueryClient({
 
 const router = createBrowserRouter([
   {
-    //헤더가 있는 디자인 페이지
-    Component: RootLayout,
+    //모바일 레이아웃 - 사용자 페이지
+    Component: MobileLayout,
     children: [
+      { path: '/', lazy: page(() => import('@/pages/Main')) },
+      { path: '/menu', lazy: page(() => import('@/pages/Menu')) },
+      { path: '/made-by', lazy: page(() => import('@/pages/MadeBy')) },
+      { path: '/lost-items', lazy: page(() => import('@/pages/LostItem/LostItem')) },
       {
-        Component: MobileLayout,
+        path: '/lost-items/new',
+        lazy: page(() => import('@/pages/LostItem/LostItemRegister')),
+      },
+      {
+        path: '/lost-items/:id/edit',
+        lazy: page(() => import('@/pages/LostItem/LostItemEdit')),
+      },
+      { path: '/lost-items/:id', lazy: page(() => import('@/pages/LostItem/LostItemDetail')) },
+      { path: '/booth-map', lazy: page(() => import('@/pages/Booth/BoothMap')) },
+      {
+        path: '/order/:boothId',
+        Component: Order,
         children: [
-          { path: '/lost-items', lazy: page(() => import('@/pages/LostItem/LostItem')) },
-          {
-            path: '/lost-items/new',
-            lazy: page(() => import('@/pages/LostItem/LostItemRegister')),
-          },
-          {
-            path: '/lost-items/:id/edit',
-            lazy: page(() => import('@/pages/LostItem/LostItemEdit')),
-          },
-          { path: '/lost-items/:id', lazy: page(() => import('@/pages/LostItem/LostItemDetail')) },
-          { path: '/booth-map', lazy: page(() => import('@/pages/Booth/BoothMap')) },
+          { index: true, lazy: page(() => import('@/pages/Order/OrderEntry')) },
+          { path: 'progress', lazy: page(() => import('@/pages/Order/OrderProgress')) },
+          { path: 'confirm', lazy: page(() => import('@/pages/Order/OrderConfirm')) },
+          { path: 'customer-info', lazy: page(() => import('@/pages/Order/CustomerInfo')) },
+          { path: 'pay', lazy: page(() => import('@/pages/Order/OrderPay')) },
+          { path: 'complete', lazy: page(() => import('@/pages/Order/OrderComplete')) },
         ],
       },
     ],
@@ -55,27 +64,6 @@ const router = createBrowserRouter([
         ],
       },
       { path: '/login', lazy: page(() => import('@/pages/admin/AdminLogin')) },
-    ],
-  },
-  {
-    //헤더 없는 사용자 페이지
-    Component: MobileLayout,
-    children: [
-      { path: '/', lazy: page(() => import('@/pages/Main')) },
-      { path: '/menu', lazy: page(() => import('@/pages/Menu')) },
-      { path: '/made-by', lazy: page(() => import('@/pages/MadeBy')) },
-      {
-        path: '/order/:boothId',
-        Component: Order,
-        children: [
-          { index: true, lazy: page(() => import('@/pages/Order/OrderEntry')) },
-          { path: 'progress', lazy: page(() => import('@/pages/Order/OrderProgress')) },
-          { path: 'confirm', lazy: page(() => import('@/pages/Order/OrderConfirm')) },
-          { path: 'customer-info', lazy: page(() => import('@/pages/Order/CustomerInfo')) },
-          { path: 'pay', lazy: page(() => import('@/pages/Order/OrderPay')) },
-          { path: 'complete', lazy: page(() => import('@/pages/Order/OrderComplete')) },
-        ],
-      },
     ],
   },
 ]);
