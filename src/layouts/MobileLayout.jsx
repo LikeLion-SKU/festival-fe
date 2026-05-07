a1a30ac9819e8859bc94747f2b29096f4f616ae8
Jeong mokjin <jungmokjin@gmail.com>
2026-05-07 16:35:57 +0900

    ♻️ Refactor: 레이아웃 구조 수정 및 미사용 파일 레이아웃 삭제

--- src/layouts/MobileLayout.jsx
+++ src/layouts/MobileLayout.jsx
@@ -9,8 +9,8 @@ export default function MobileLayout() {
 
   return (
     <main className="flex h-full flex-col">
-      <div className="bg-gray-50 min-h-0 flex-1">
-        <div className="relative mx-auto w-full max-w-112.5 h-full bg-white shadow-lg">
+      <div className=" min-h-0 flex-1">
+        <div className="relative mx-auto w-full max-w-112.5 h-full bg-white">
           <Outlet context={{ onModalChange: setIsModalOpen, isModalOpen, setIsLoading }} />
           {isLoading && <Loading />}
         </div>
